5a84c822b2f2f1b17f16eb2a361e7cd85bdf8d2b
COMLAB <Kpangilinan23-0350@cca.edu.ph>
2025-02-17 17:52:36 +0800

    Javascript form

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+		<head> <title> 
+		Javascript form 
+		</title>
+		</head>
+	<body style="background-color: black; color: #fff;">
+		<h1>Student Record</h1>
+		<table id="student_table">
+		</table>
+		
+		<script>
+			const studentTable = document
+				.getElementById("student_table")
+			
+			console.log(studentTable);
+		</script>
+	</body>
+</html>
